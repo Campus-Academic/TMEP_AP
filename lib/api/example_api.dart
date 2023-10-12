5779f30d7297811f633c5ebf173a155a225886f4
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-10-12 20:21:04 +0800

    fix: get_class_schedule_ex

--- lib/api/example_api.dart
+++ lib/api/example_api.dart
@@ -1,6 +1,9 @@
 import 'package:app_template_v0/config/index.dart';
+import 'package:app_template_v0/interface/index.dart';
 
-Future<String> get_class_schedule_ex(String uid, String password) async {
-  var timeout = receiveTimeout;
-  return true;
+Future<List<Course>> get_class_schedule_ex(String uid, String password) async {
+  // 使用conf 裡面的常數
+  final List<Course> courses = [];
+
+  return courses;
 }
