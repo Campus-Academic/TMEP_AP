b97d61c3f15f1c5c54b9b6e513c05c2e62f6ab10
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-10-12 20:22:35 +0800

    feat: add dependencies

--- lib/api/example_api.dart
+++ lib/api/example_api.dart
@@ -1,9 +1,12 @@
 import 'package:app_template_v0/config/index.dart';
 import 'package:app_template_v0/interface/index.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as htmlParser;
 
 Future<List<Course>> get_class_schedule_ex(String uid, String password) async {
   // 使用conf 裡面的常數
   final List<Course> courses = [];
 
+  // TODO: 加入其他邏輯 .....
   return courses;
 }
