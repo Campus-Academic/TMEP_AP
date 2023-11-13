2a168002b8ca72df0f752b58134e7b4ad45ac664
s30595@gmail.com <s30595@gmail.com>
2023-11-13 15:27:32 +0800

    Update navigation bar

--- lib/presentation/layout/app_home_nav_page.dart
+++ lib/presentation/layout/app_home_nav_page.dart
@@ -1,9 +1,9 @@
-
-import 'package:flutter/material.dart';
-import 'package:auto_route/auto_route.dart';
 import 'package:app_template_v0/presentation/core/style/color.dart';
-import 'package:app_template_v0/presentation/widgets/bar/nav_bar/bottom_nav_bar.dart'; // 引入自定義底部導航欄
+import 'package:app_template_v0/presentation/layout/app_home_nav_bar_widget.dart';
 import 'package:app_template_v0/presentation/router/app_router.gr.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class AppHomeNavPage extends StatelessWidget {
@@ -17,23 +17,47 @@ class AppHomeNavPage extends StatelessWidget {
       const OtherFunctionsRoute(),
     ];
 
-    // 這裡我們將使用自定義的底部導航欄
-    return WillPopScope(
+    final List<BottomNavigationBarItem> items = <BottomNavigationBarItem>[
+      const BottomNavigationBarItem(
+        label: 'schedule',
+        icon: Icon(Icons.class_),
+      ),
+      const BottomNavigationBarItem(
+        label: 'home',
+        icon: Icon(Icons.home),
+      ),
+      const BottomNavigationBarItem(
+        label: 'others',
+        icon: Icon(Icons.functions_rounded),
+      ),
+    ];
+
+    appHomeInit(context);
+
+    return
+        // MultiBlocProvider(
+        //   providers: [],
+        //   child: MultiBlocListener(
+        //     listeners: [],
+        //     child:
+        WillPopScope(
       onWillPop: () async => false,
       child: AutoTabsScaffold(
         backgroundColor: ColorStyle.beige,
         routes: routes,
         bottomNavigationBuilder: (_, tabsRouter) {
-          return CustomBottomNavigationBar(
-            isLogged: false, // 根據您的應用邏輯設置
-            selectedIndex: tabsRouter.activeIndex, // 使用tabsRouter的activeIndex
-            onItemSelected: (index) {
-              tabsRouter.setActiveIndex(index); // 切換頁面
-            },
-            userImagePath: "assets/images/user_image.png", // 根據您的應用邏輯設置
+          return AppHomeNavBarWidget(
+            items: items,
+            tabsRouter: tabsRouter,
           );
         },
       ),
+      //   ),
+      // ),
     );
   }
+}
+
+void appHomeInit(BuildContext context) {
+  // TODO: init bloc, fetch data...
 }
